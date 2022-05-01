4f7958e5e18ce39bbafcce9b846585051eedd146
Dandix <dandix.rus@yandex.ru>
2022-05-01 21:45:00 +0800

    изменение enemy скрипта

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform spawnPoint; // точка респавна зомби
+    public GameObject player; // главный герой
+
+    public float speed;
+    public float enemyHP = 3f;
+
     void Start()
     {
+        transform.position = spawnPoint.position;
         
+
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+
+        transform.position = Vector3.MoveTowards(transform.position,player.transform.position, speed * Time.deltaTime); // враг идет за игроком.
+        if (enemyHP <= 0)
+        {
+            Destroy(gameObject);
+
+        }
+
         
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+          
+
+        }
     }
 }
